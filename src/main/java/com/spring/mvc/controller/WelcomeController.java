fb021a6e6fd2a226b287a4b94cb2b2a070e5e9ce
Neelanshu Pandey <neelanshupandey1@gmail.com>
2026-03-17 18:45:25 +0530

    uupdate

--- src/main/java/com/spring/mvc/controller/WelcomeController.java
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -1,19 +1,29 @@
 package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 @Controller
 public class WelcomeController {
 
-	
-	@RequestMapping("/")
-	public String meeting() {
-		
-		
-		System.out.println("welcome controller");
+	@GetMapping("/")
+	public String greeting() {
+		System.out.println("WelcomeController.greeting()");
 		return "index";
 	}
+
+	@GetMapping("/req-param")
+	public String createUser(@RequestParam(name = "user") String name, Model model) {
+		
+		model.addAttribute("user", name);
+
+		System.out.println("WelcomeController.greeting :" + name);
+
+		return "success";
+	}
+
 	
 	
 }
