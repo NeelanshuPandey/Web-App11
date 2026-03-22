9a5bc9c64ccf48a875217e0975e711bf30ae9513
Neelanshu Pandey <neelanshupandey1@gmail.com>
2026-03-22 11:47:26 +0530

    update

--- src/main/java/com/spring/mvc/controller/WelcomeController.java
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -2,6 +2,7 @@ package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,7 +26,17 @@ public class WelcomeController {
 
 		return "success";
 	}
+	@GetMapping("/path-var/{id}") // 1-read data from url by @GetMapping and store in {id} and then @PathVariable take data from @GetMapping
+	public String pathVariable(@PathVariable(name = "id") int id, Model model) {
+		
+		//path variable read the data from end point of url
+		
+		model.addAttribute("user","sorry this:"+id+" user is not found our db");
 
+		System.out.println("WelcomeController.PathVariable");
+
+		return "success";
+	}
 	
 	
 }
