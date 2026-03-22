943a81d789ab6ba07b6070cec072eee2cf9cdf6b
Neelanshu Pandey <neelanshupandey1@gmail.com>
2026-03-22 11:10:16 +0530

    update

--- src/main/java/com/spring/mvc/controller/WelcomeController.java
+++ src/main/java/com/spring/mvc/controller/WelcomeController.java
@@ -2,6 +2,7 @@ package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
@@ -14,8 +15,9 @@ public class WelcomeController {
 		return "index";
 	}
 
-	@GetMapping("/req-param")
-	public String createUser(@RequestParam(name = "user") String name, Model model) {
+
+	@GetMapping("/req-param")   //give input in url (/req-param?user=Ashish)
+	public String createUser(@RequestParam(name = "user",defaultValue="my-default-name") String name, Model model) {
 		
 		model.addAttribute("user", name);
 
